db1a43526b7def6ea8bc54b5a1cc2f49a57e53d2
Benmerabet Ayoub <62332376+ay19b@users.noreply.github.com>
2023-04-09 15:22:17 +0100

    update

--- client/src/component/createProd/create.js
+++ client/src/component/createProd/create.js
@@ -35,7 +35,7 @@ export default function Create() {
     const [errorPrice, setErrorPrice] = useState(false);
     const [errorStock, setErrorStock] = useState(false);
     const [errorCatg, setErrorCatg] = useState(false);
-    const api = "http://localhost:3001";
+    const api = "https://dashboard-api-v8p2.onrender.com"; 
     const navigate = useNavigate();
 
       const handleFileInput = (event) => {
